8c9c2143eb6ac24e1788f3a622524ebdfdcd421c
meomaymap <79776624+meomaymap@users.noreply.github.com>
2021-06-22 23:33:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var DisplaySize = [[992, 576, 1], 
                   [5000,991,575]];
-var tyle = [4,3,1];
+var tyle1 = [3.6,2.2,1.1];
+var tyle2 = [2.5,2,1];
 const root = document.querySelector(':root')
 
 displayWindowSize();
@@ -9,7 +10,8 @@ function displayWindowSize() {
 
   for(var i=0; i<DisplaySize[0].length;i++){
       if(window.innerWidth > DisplaySize[0][i] && window.innerWidth <= DisplaySize[1][i]) {
-          root.style.setProperty('--tyle1', tyle[i]);
+          root.style.setProperty('--tyle1', tyle1[i]);
+          root.style.setProperty('--tyle2', tyle2[i]);
       }
   }
   
